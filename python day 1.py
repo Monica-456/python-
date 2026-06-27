592a8f221a98f8e855ccd7c4a3d98c2c20b26894
Monica-456 <ksatyamonica@gmail.com>
2026-06-27 19:37:48 +0530

    Add files via upload
    dealt with various operators in python with examples.

---
+++ python day 1.py
@@ -0,0 +1,106 @@
+'''operators
+1 Arthematic Operators
+ +,-,*,/,%,
+
+2 Assignment Operators
++=(used for increment),-=(used for decremenet),*=
+
+3 Comparision Operators
+  ==,<=,>=,!=, >,<
+ 
+eg= a=20
+    b=20
+    print(a==b)
+ true
+
+a=20
+b=20
+print(a!=b)
+false
+
+a=20
+b=7
+print(a>=b)
+true
+
+a=20
+b=7
+print(a<=b)
+false
+
+4 Logical Opertaors :- 
+ And - returns true if both the conditions are true.
+ Or - returns true if any one condition is correct.
+ Not - returns true if both the conditions are false(reverse output)
+
+eg= a=20
+b=7
+c=89
+print(a>=b and b<=c)
+true
+
+5 Identity Operator
+is:- same object or not
+is not :- is not the same object
+[DIFF  BTW == AND is is that == look for the values and is look for the object(memory location)
+
+ Eg= a=20
+     b=20
+    print(a==b)
+    print(a is b)
+output= true
+        true
+
+      a=20
+      b=2
+      print(a==b)
+      print(a is b)
+       
+output= false
+        false
+
+6 Membership Operator
+ in = returns true if a specified value is found inside a sequence 
+ not in =returns true if a specified value is not found inside a sequence 
+
+
+ 7 Bitwise operator
+ &,<<,>>,! ^
+
+print(5 & 4)
+print(5 ! 4)
+'''
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+ 
